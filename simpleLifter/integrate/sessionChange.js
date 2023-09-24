f05ea22220740f25cd46c3f7a8a0152e21ef3e25
meethed <121379364+meethed@users.noreply.github.com>
2023-09-24 21:07:26 +1000

    Create sessionChange.js
    javascript for changing the session

---
+++ simpleLifter/integrate/sessionChange.js
@@ -0,0 +1,15 @@
+function doSessionChange() {
+//TODO - all of it
+
+//get list of active comps
+const activeComps=[];
+var accessCode,
+    compLetters;
+//show dialog with dropdown of active comps
+
+//validate session
+
+//reload
+
+}
+
