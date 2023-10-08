e7243539a3f9ee174eacb3aaea93adea1c5d38bf
meethed <121379364+meethed@users.noreply.github.com>
2023-10-08 17:29:22 +1100

    Update windows.js
    added the f5 bit where you don't get a new window

--- simpleLifter/integrate/windows.js
+++ simpleLifter/integrate/windows.js
@@ -1,6 +1,9 @@
 var dolightWindow=0, doplateWindow=0, lightWindow, plateWindow;
 //lightWindow=window.open("popupLights.html","","menubar=no,scrollbars=no,status=no,toolbar=no");
-lightWindow=window.open("../../plateLights.php","","menubar=no,scrollbars=no,status=no,toolbar=no,width=1920,height=1080"); 
+lightWindow=window.open("","light_window_popup","menubar=no,scrollbars=no,status=no,toolbar=no,width=1920,height=1080");
+if (lightWindow.location.href === "about:blank") {
+lightWindow=window.open("../../plateLights.php","light_window_popup","menubar=no,scrollbars=no,status=no,toolbar=no,width=1920,height=1080");
+}
 dolightWindow=1;
 //plateWindow=window.open("popupPlates.html","","menubar=no,scrollbars=no,status=no,toolbar=no");
 doplateWindow=1;
