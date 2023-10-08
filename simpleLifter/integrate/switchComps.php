bf76663b5d53afc3a58ac5db94c53827ef10bf99
meethed <121379364+meethed@users.noreply.github.com>
2023-10-08 17:28:41 +1100

    Update switchComps.php
    fixed copy and paste from putty error

--- simpleLifter/integrate/switchComps.php
+++ simpleLifter/integrate/switchComps.php
@@ -6,12 +6,12 @@ $conn = new mysqli('localhost', 'lightsuser','lights','lightsdb');
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT compLetters, compID, compName, hish FROM comps WHERE compLetters$
+$sql = "SELECT compLetters, compID, compName, hish FROM comps WHERE compLetters=\"" . $_POST["compName"]. "\"";
 $result = $conn->query($sql);
 
 
-if ($result->num_rows > 0)  //if there's a competition that matches the compLet$
-  // competition so if the database is corrupted this will be a bit messy but t$
+$sql = "SELECT compLetters, compID, compName, hish FROM comps WHERE compLetters=\"" . $_POST["compName"]. "\"";
+  // competition so if the database is corrupted this will be a bit messy but that can't happen as that isn't
   // how the db is set up
   // output data of each row
   while($row = $result->fetch_assoc())
