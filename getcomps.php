6b26443645219d9e3dbe95363aa8ddb52608f272
meethed <121379364+meethed@users.noreply.github.com>
2023-10-19 10:44:06 +1100

    Update getcomps.php
    if you provide  a c=xxx in the GET URL then it will return all sibling competitions only.
    
    if you provide nothing, it will return all upcoming comps. so if you're changing session, it will only allow you to switch between sessions of the same comp, rather than a free for all

--- getcomps.php
+++ getcomps.php
@@ -1,6 +1,18 @@
 <?php 
 include_once "../config.php";
- $stmt=$conn->prepare("SELECT  compName, compLetters from comps WHERE enddate >= curdate()"); 
+ if ($cc=filter_input(INPUT_GET, "c", FILTER_SANITIZE_STRING) ) {
+  $stmt=$conn->prepare("SELECT parentComp from comps where compLetters = ?");
+  $stmt->bind_param("s",$cc);
+  $stmt->execute();
+  $result = $stmt->get_result()->fetch_assoc();
+
+  $stmt=$conn->prepare("SELECT compName,compLetters from comps where parentComp=? AND enddate >= curdate() AND NOT compLetters=?");
+  $stmt->bind_param("ss",$result["parentComp"],$result["parentComp"]);
+
+} else {
+ $stmt=$conn->prepare("SELECT isChild,parentComp,isParent, compName, compLetters from comps WHERE enddate >= curdate()");
+}
+
   $stmt->execute(); 
   $result = $stmt->get_result(); 
   while ($row =  $result->fetch_assoc()) {
