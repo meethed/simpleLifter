d7e20f34e55d7c63940b075b2f54a57a1f94f837
meethed <121379364+meethed@users.noreply.github.com>
2024-04-17 20:41:32 +1000

    Delete simpleLifter directory

--- simpleLifter/integrate/setpwd.php
+++
@@ -1,8 +0,0 @@
-<?php
-$c=$_GET["c"];
-$p=$_GET["p"];
-$cpwd = crypt($p,substr($c,1,2));
-
-echo $cpwd;
-
-?>
--- simpleLifter/integrate/windows.js
+++
@@ -1,9 +0,0 @@
-var dolightWindow=0, doplateWindow=0, lightWindow, plateWindow;
-//lightWindow=window.open("popupLights.html","","menubar=no,scrollbars=no,status=no,toolbar=no");
-lightWindow=window.open("","light_window_popup","menubar=no,scrollbars=no,status=no,toolbar=no,width=1920,height=1080");
-if (lightWindow.location.href === "about:blank") {
-lightWindow=window.open("../../plateLights.php","light_window_popup","menubar=no,scrollbars=no,status=no,toolbar=no,width=1920,height=1080"); 
-}
-dolightWindow=1;
-//plateWindow=window.open("popupPlates.html","","menubar=no,scrollbars=no,status=no,toolbar=no");
-doplateWindow=1;
